74d552fffc903414cff6cafecdbd80d5f285103f
zavsnar <zavsnar@gmail.com>
2013-03-19 14:19:41 +0400

    Add config of nginx

---
+++ deploy/prepare_fs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Prepare filesystem (mkdir), install virtualenv
+
+cd `dirname "$0"`;
+BRANCH_ROOT="../../"
+USER_NAME=$USER
+
+make_dirs(){
+    (
+        cd $BRANCH_ROOT
+        echo -n "Creating folders..."
+        mkdir -p \
+                static \
+                media \
+                logs \
+            && echo "  ok" \
+            || echo "  ERROR!" 1>&2;
+    )
+}
+
+if [ "$1" ]; then
+    make_dirs;
+    # echo "Creating virtual python environment..."
+    # ./env_update.sh "$1" \
+    #     && echo "  ok" \
+    #     || echo "  ERROR!" 1>&2;
+    cd "$BRANCH_ROOT";
+    REALPATH=$(readlink -f local_settings.py)
+    if [ ! -e $REALPATH ]; then
+        echo "from default_local_settings import *" > local_settings.py
+    fi;
+else
+   echo "Error: python version requires as first argument" 1>&2;
+fi
